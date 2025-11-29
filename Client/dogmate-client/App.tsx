34c172da16fc2950104e04c3ffc7b5df807bc49e
muadiom <muadiom@post.bgu.ac.il>
2025-11-29 14:25:59 +0200

    something changed

--- Client/dogmate-client/App.tsx
+++ Client/dogmate-client/App.tsx
@@ -1,23 +1,3 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-// import HomeScreen from './screens/HomeScreen'
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <HomeScreen  />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
 // App.tsx
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
